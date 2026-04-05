fc3a094cf9f594e94339d188cef1c27e4c1a78ed
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-04-05 12:12:41 +0800

    Update database.php

--- Admin/database.php
+++ Admin/database.php
@@ -2,7 +2,7 @@
 // Mengambil data dari environment variables Railway
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
-$pass = getenv('MYSQLPASSWORD') ?: "dftQWOjFdpLtcZkGUZzgMOsluGVBBiBD";
+$pass = getenv('MYSQLPASSWORD') ?: "pYTBAlaxErAlOwXxdoKDmJkJGnratqWD";
 $db   = getenv('MYSQLDATABASE') ?: "railway";
 $port = getenv('MYSQLPORT') ?: "3306";
 
