9834839480227923606b4f662ce165b0c6cd5e1a
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-04-05 13:28:51 +0800

    Update dashboard.php

--- Admin/content/dashboard.php
+++ Admin/content/dashboard.php
@@ -39,7 +39,6 @@
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -59,7 +58,7 @@
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        
                     </div>
                 </div>
 
@@ -83,7 +82,6 @@
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
         </div><!-- /.container-fluid -->
