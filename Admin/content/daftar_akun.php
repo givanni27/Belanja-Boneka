a8258e3b2e3f296c0a1edfeec13ffe7f4cab19bd
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-04-07 15:32:52 +0800

    Update daftar_akun.php

--- Admin/content/daftar_akun.php
+++ Admin/content/daftar_akun.php
@@ -85,7 +85,7 @@
                                             <i class="fas fa-pencil-alt"></i>
                                         </a>
                                         <a class="btn btn-danger btn-sm"
-                                            href="proses/proses_tambah_akun.php?proses=delete&id_users=<?php echo $row['id_users']; ?>"
+                                            href="proses/proses_tambah_akun.php?proses=delete&id_users=<?php echo $row['id_users']; ?> &nama_pembeli=<?php echo $row['nama_pembeli']; ?>"
                                             onclick="return confirm('Yakin hapus?')" title="Delete">
                                             <i class="fas fa-trash"></i>
                                         </a>
